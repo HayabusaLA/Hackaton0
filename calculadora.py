77c7aedbc5ffc81365f9fcbd07e3c390a7fa3a73
Giordano Fuentes <giordimega@hotmail.com>
2024-08-31 17:17:55 -0500

    multi

---
+++ calculadora.py
@@ -0,0 +1,32 @@
+import re
+
+def extraer_componentes(operacion):
+    # Expresión regular para extraer factores y operador
+    match = re.match(r'(\d+)\s*([\+\-\*/])\s*(\d+)', operacion)
+    if match:
+        factor1, operador, factor2 = match.groups()
+        return factor1, operador, factor2
+    else:
+        return None, None, None
+op=input("operacion:")
+
+factor1, operador, factor2 = extraer_componentes(op)
+
+def multiplicacion(a,b):
+    return a*b
+
+def calcular_resultado(factor1, operador, factor2):
+    # Convertir los factores a números enteros
+    num1 = int(factor1)
+    num2 = int(factor2)
+    
+    if operador == '+':
+        return (num1, num2)
+    elif operador == '-':
+        return resta(num1, num2)
+    elif operador == '*':
+        return multiplicacion(num1, num2)
+    elif operador == '/':
+        return dividcion(num1, num2)
+    else:
+        return "Operador no reconocido"
